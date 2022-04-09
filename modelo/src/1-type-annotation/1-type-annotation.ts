bbe69137c71e595978a04ec51a83bbacc2ef6c9f
Leonardo <leoleitao27@gmail.com>
2022-04-09 20:20:38 -0300

    aula 1

--- modelo/src/1-type-annotation/1-type-annotation.ts
+++ modelo/src/1-type-annotation/1-type-annotation.ts
@@ -1,2 +1,24 @@
 /* eslint-disable */
-let nome = 'Luiz';
+let nome: string = 'Luiz';
+let idade: number = 30;
+let adulto:boolean = true;
+let simbolo: symbol = Symbol('qualquer-symbol');
+
+/* array */
+let arrayDeNumero: Array<number> = [1,2,3];
+let arrayDeNumero2: number[] = [1,2,3];
+let arrayDeStrings: Array<string> = ['leo', 'aaa'];
+let arrayDeStrings2: string[] = ['leo', 'aaa'];
+
+/* Objetos */
+let pessoa: {nome: string, idade:number, adulto?:boolean} = {
+idade: 30,
+nome: 'leo',
+/* O tipo adulto foi declarado como não obrigatório */
+};
+
+function soma(x: number, y:number):number {
+    return x + y;
+}
+
+const soma2: (x:number, y:number) => number = (x,y) => x+y;
