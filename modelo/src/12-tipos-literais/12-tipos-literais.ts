63cd5c28529eb036dfcf98f3b3224a74917d62c5
Leonardo <leoleitao27@gmail.com>
2022-04-12 13:07:28 -0300

    aula 12

---
+++ modelo/src/12-tipos-literais/12-tipos-literais.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+let x = 10;
+
+const pessoa = {
+  nome: 'luiz',
+  sobrenome: 'miranda',
+}
+
+pessoa.nome = 'leo';
+console.log(pessoa);
+
+//module mode
+export default 1;
+//saindo do escopo global
