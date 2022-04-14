ad6f3d7c0cdedae50656391a49f74de18fe10691
leonardo <leoleitao27@gmail.com>
2022-04-14 10:58:54 -0300

    aula 13

---
+++ modelo/src/13-type-alias/13-type-alias.ts
@@ -0,0 +1,24 @@
+/* Usamos o type alias para  apelidar tipos */
+type Idade = number;
+type Pessoa = {
+  nome: string;
+  idade: Idade;
+  salario: number;
+  corPreferida?: string;
+};
+
+type CorRGB = 'vermelho' | 'verde' | 'azul';
+type CorCMYK = 'ciano' | 'magenta' | 'amarelo' | 'preto';
+type CorPreferida = CorRGB | CorCMYK;
+
+const pessoa: Pessoa = {
+  idade: 30,
+  nome: 'leo',
+  salario: 20000,
+};
+
+export function setCorPreferida(pessoa: Pessoa, cor: CorPreferida): Pessoa {
+  return { ...pessoa, corPreferida: cor };
+}
+
+console.log(setCorPreferida(pessoa, 'vermelho'));
