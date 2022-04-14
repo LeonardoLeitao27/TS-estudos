8619fecc8389589c3aa4738096ba3173770c22ee
leonardo <leoleitao27@gmail.com>
2022-04-14 12:33:57 -0300

    aula 15

---
+++ modelo/src/15-funcoes-como-tipo/15-funcoes-como-tipo.ts
@@ -0,0 +1,16 @@
+type MapStrignsCallback = (item: string) => string;
+
+export function mapStrings(array: string[], callbackfn: MapStrignsCallback): string[] {
+  const newArray: string[] = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i];
+    newArray.push(callbackfn(item));
+  }
+
+  return newArray;
+}
+
+const abc = ['a', 'b', 'c'];
+const abcMapped = mapStrings(abc, (item) => item.toUpperCase());
+console.log(abcMapped);
